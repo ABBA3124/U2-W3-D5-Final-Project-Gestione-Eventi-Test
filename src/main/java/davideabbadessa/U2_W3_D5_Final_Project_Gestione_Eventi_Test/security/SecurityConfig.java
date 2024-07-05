384ace0362170f6f2bc4508672e95c2469ee2007
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-05 14:06:17 +0200

    Configurazione della sicurezza con JWT e filtro di autenticazione
    - Configurazione del filtro JWTAuthFilter per autenticare le richieste.
    - Implementazione della classe JWTTools per la generazione e la verifica dei token JWT.
    - Configurazione della classe SecurityConfig per gestire le autorizzazioni e disabilitare il form login.

---
+++ src/main/java/davideabbadessa/U2_W3_D5_Final_Project_Gestione_Eventi_Test/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.security;
+
+
+import davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin(http -> http.disable());
+        httpSecurity.csrf(http -> http.disable());
+        httpSecurity.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    PasswordEncoder getBCrypt() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+
+}
