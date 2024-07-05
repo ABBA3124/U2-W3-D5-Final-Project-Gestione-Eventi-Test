2a81af3b119fff94352aa6bbf2f729f74fcc51b4
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-05 11:41:50 +0200

    Add Entity User
    -Creato un enum chiamato role
    -creato user con chiave primaria , assegnato enum role, definita relazione OneToMany,
    
    - definita relazione ManyToMany tra l'entità User e l'entità Event (un utente può partecipare a molti eventi e un evento può avere molti partecipanti.).
    joinColumns e inverseJoinColumns utilizzate per definire le colonne della tabella di join che saranno utilizzate per memorizzare le chiavi primarie delle due entità coinvolte nella relazione.
    
    -definito Set<Event> bookedEvents è un insieme (set) di oggetti Event che rappresentano tutti gli eventi ai quali l'utente ha prenotato la partecipazione.
    Utilizzando un Set assicuriamo che non ci siano duplicati.

---
+++ src/main/java/davideabbadessa/U2_W3_D5_Final_Project_Gestione_Eventi_Test/entities/User.java
@@ -0,0 +1,39 @@
+package davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.entities;
+
+
+import davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.enums.Role;
+import jakarta.persistence.*;
+import jdk.jfr.Event;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@ToString
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "creator")
+    private Set<Event> createdEvents;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_event",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id"))
+    private Set<Event> bookedEvents;
+}
---
+++ src/main/java/davideabbadessa/U2_W3_D5_Final_Project_Gestione_Eventi_Test/enums/Role.java
@@ -0,0 +1,5 @@
+package davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.enums;
+
+public enum Role {
+    USER, ORGANIZER
+}
