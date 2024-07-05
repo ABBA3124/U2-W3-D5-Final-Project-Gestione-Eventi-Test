1882170f06150dbb87d21237e14cc762665a9927
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-05 14:12:42 +0200

    Implementazione del servizio per la gestione degli eventi
    - Creazione dei metodi nel EventService per la gestione CRUD degli eventi (creazione, aggiornamento, eliminazione, recupero).

--- src/main/java/davideabbadessa/U2_W3_D5_Final_Project_Gestione_Eventi_Test/services/EventService.java
+++ src/main/java/davideabbadessa/U2_W3_D5_Final_Project_Gestione_Eventi_Test/services/EventService.java
@@ -2,6 +2,7 @@ package davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.services;
 
 
 import davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.entities.Event;
+import davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.exceptions.NotFoundException;
 import davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,6 @@ import java.util.UUID;
 
 @Service
 public class EventService {
-
     @Autowired
     private EventRepository eventRepository;
 
@@ -19,11 +19,15 @@ public class EventService {
         return eventRepository.findAll();
     }
 
-    public Event save(Event event) {
+    public Event saveEvent(Event event) {
         return eventRepository.save(event);
     }
 
-    public void delete(UUID id) {
+    public void deleteEvent(UUID id) {
         eventRepository.deleteById(id);
+    }
+
+    public Event findEventById(UUID id) {
+        return eventRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
     }
 }
