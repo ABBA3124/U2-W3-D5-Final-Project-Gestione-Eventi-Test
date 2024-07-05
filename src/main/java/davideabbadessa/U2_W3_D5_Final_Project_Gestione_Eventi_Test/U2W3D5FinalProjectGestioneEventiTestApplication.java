2be0dbba8be08c5fe3f9eeace48e647368b9a99e
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-05 11:24:51 +0200

    Start Progect

---
+++ src/main/java/davideabbadessa/U2_W3_D5_Final_Project_Gestione_Eventi_Test/U2W3D5FinalProjectGestioneEventiTestApplication.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W3D5FinalProjectGestioneEventiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W3D5FinalProjectGestioneEventiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davideabbadessa/U2_W3_D5_Final_Project_Gestione_Eventi_Test/U2W3D5FinalProjectGestioneEventiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W3D5FinalProjectGestioneEventiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
