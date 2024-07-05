adeb026fd883bb53f0ef21b65939781f30e30306
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-05 11:45:28 +0200

    Aggiunta entità Event
    - Creata classe Event con annotazioni JPA per mappatura alla tabella 'event' nel database
    - Inclusi campi: id, title, description, date, location e availableSeats
    - Configurata relazione Many-to-One con l'entità User per il creatore dell'evento

---
+++ src/main/java/davideabbadessa/U2_W3_D5_Final_Project_Gestione_Eventi_Test/entities/Event.java
@@ -0,0 +1,30 @@
+package davideabbadessa.U2_W3_D5_Final_Project_Gestione_Eventi_Test.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@ToString
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String title;
+    private String description;
+    private LocalDateTime date;
+    private String location;
+    private int availableSeats;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id")
+    private User creator;
+}
